aec798e83bcc874487c635f3c63c9798c298db5a
VeeBee <vikas@triamis.com>
2023-07-20 16:41:44 +0800

    Initial Commit

---
+++ Flower/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flower
+//
+//  Created by Vikas Bhandari on 20/7/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flower/FlowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerApp.swift
+//  Flower
+//
+//  Created by Vikas Bhandari on 20/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
